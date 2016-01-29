f5abd68393de0e7b3cff05ddaa6868052e1a4cb8
wasao <wasao@ac-room.org>
2016-01-29 17:42:33 +0900

    Add tools/sten.scm

---
+++ tools/sten.scm
@@ -0,0 +1,34 @@
+;;;
+;;; Scheme template engine
+;;;
+
+(use file.util)
+
+(define (sten-eval str)
+  (with-output-to-string
+    (^[] (eval (read-from-string str)
+               (current-module)))))
+
+(define (sten string)
+  (regexp-replace-all
+   #/<SCM>(.*?)<\/SCM>/
+   string
+   (^[x] (sten-eval (x 1)))))
+
+(define (save file string)
+  (with-output-to-file file
+    (^[] (display string))))
+
+(define (usage)
+  (display
+   "Usage: sten [input] [output]\n"
+   (current-error-port)))
+
+(define (main args)
+  (unless (= (length args) 3)
+    (usage)
+    (sys-exit -1))
+
+  (let ([in  (~ args 1)]
+        [out (~ args 2)])
+    (save out (sten (file->string in)))))
